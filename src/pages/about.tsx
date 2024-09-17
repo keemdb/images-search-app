7b4b309da64ff239e79ed68b42956af6d3533bfd
behind-code <behind.lv@gmail.com>
2024-09-18 03:01:18 +0900

    route 설정

---
+++ src/pages/about.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function about() {
+  return (
+	<div>about 페이지</div>
+  )
+}
+
+export default about
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function index() {
+  return (
+	<div>메인 페이지</div>
+  )
+}
+
+export default index
